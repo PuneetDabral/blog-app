7a04d1c9b613095977ad17a9623815d2bf302e65
Puneet Dabral <puneetdabral2000@gmail.com>
2022-04-19 07:33:20 +0530

    add routes and link to replace ancher tags

--- blogger/src/App.js
+++ blogger/src/App.js
@@ -1,15 +1,27 @@
 import Navbar from './Components/NavBar';
 import Home from './Components/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from './Components/Create';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+    <Routes>
+    <Route path="/"  element= { <Home /> } />
+    </Routes>
       </div>
+      <Routes>
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
+
+
+
 
 export default App;
